e788e95a7a2269235d7bda50d316c89a8b8b5b44
Seligmann <zachary.a.seligman@gmail.com>
2022-02-18 00:02:38 -0600

    fixed to use blocking for now since we're using async version of the get api

--- src/src/main.rs
+++ src/src/main.rs
@@ -3,13 +3,14 @@ extern crate select;
 
 use select::document::Document;
 use select::predicate::Name;
+use reqwest::blocking;
 
 fn main() {
     over_rustle("https://news.ycombinator.com");
 }
 
 fn over_rustle(url: &str) {
-    let mut resp = reqwest::get(url).unwrap();
+    let mut resp = reqwest::blocking::get(url).unwrap();
     assert!(resp.status().is_success());
 
     Document::from_read(resp)
