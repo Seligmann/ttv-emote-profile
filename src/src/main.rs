84a139a0cff4bee8d8d62d4f6b2a2321dc7b47d1
Seligmann <zachary.a.seligman@gmail.com>
2022-02-22 12:13:55 -0600

    previously overcomplicated. just download txt file

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,51 +1,15 @@
 extern crate reqwest;
 extern crate scraper;
 
-// use scraper::Html;
-// use soup::prelude::*;
-// use scraper::{Html, Selector};
-use reqwest::StatusCode;
+use std::io;
+use std::fs::File;
 
-mod utils;
-
-#[tokio::main]
-async fn main() {
-    let client = utils::get_client();
-    let url = "https://overrustlelogs.net/stalk?channel=Destinygg&nick=Ze1ig";
-    let result = client.get(url).send().await.unwrap();
-
-    let raw_html = match result.status() {
-        StatusCode::OK => result.text().await.unwrap(),
-        _ => panic!("LOOOOLL BAD BAD BAD"),
-    };
-
-    println!("HTML: {}", raw_html);
+fn main() {
+    let resp = reqwest::blocking::get("https://overrustlelogs.net/Destinygg%20chatlog/February%202022/2022-02-22.txt")
+        .expect("request failed");
+    let body = resp.text().expect("body invalid");
+    let mut out = File::create("2022-02-22.txt")
+        .expect("failed to create file");
+    io::copy(&mut body.as_bytes(), &mut out)
+        .expect("failed to copy content");
 }
-
-// fn des_tiny(url: &str) {
-    // for item in li_tags {
-    //     let item_text = item.text().collect::<Vec<_>>();
-    //     println!("{:?}", item_text);
-    // }
-
-    // let resp = reqwest::blocking::get(url).unwrap();
-    // assert!(resp.status().is_success());
-    //
-    // let body = resp.text().unwrap();
-    //
-    // let soup = Soup::new(&body);
-    // let div = soup.tag("div")
-    //     .find()
-    //     .expect("Couldn't find tag 'div'");
-    // let mut span = div.children().filter(|child| child.is_element());
-    // println!("{:?}", span.next().map(|tag| tag.next().to_string()));
-
-    // let fragment = Html::parse_document(&body);
-    // let memes = Selector::parse(".text").unwrap();
-    //
-    // for element in fragment.select(&memes) {
-    //     let element_txt = element.text().collect::<Vec<_>>();
-    //     println!("{:?}", element_txt[0]);
-    // }
-// }
-
