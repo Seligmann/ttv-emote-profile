98703d31ba338a0caf4b491faf400c814d35762d
Seligmann <zachary.a.seligman@gmail.com>
2022-02-22 13:23:18 -0600

    a bit of refactoring

--- src/src/main.rs
+++ src/src/main.rs
@@ -5,11 +5,18 @@ use std::io;
 use std::fs::File;
 
 fn main() {
-    let resp = reqwest::blocking::get("https://overrustlelogs.net/Destinygg%20chatlog/February%202022/2022-02-22.txt")
-        .expect("request failed");
-    let body = resp.text().expect("body invalid");
-    let mut out = File::create("2022-02-22.txt")
-        .expect("failed to create file");
-    io::copy(&mut body.as_bytes(), &mut out)
-        .expect("failed to copy content");
+    download("https://overrustlelogs.net/Destinygg%20chatlog/February%202022/2022-02-22.txt");
+}
+
+/*
+FIXME add automatic parsing of url to get the date, which will be the name of the file that is
+created
+*/
+
+fn download(url: &str) {
+    let url = String::from(url);
+    let resp = reqwest::blocking::get(url).expect("Failed to get url");
+    let body = resp.text().expect("Body is invalid");
+    let mut out = File::create("2022-02.22-txt").expect("Failed to create file");
+    io::copy(&mut body.as_bytes(), &mut out).expect("Failed to copy content");
 }
