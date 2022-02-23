44b2e996374c2f44550ba6b8e304d163d8e052fd
Seligmann <zachary.a.seligman@gmail.com>
2022-02-23 01:21:17 -0600

    structs for user, emoteinfo, imageinfo

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,24 +1,30 @@
 extern crate reqwest;
 extern crate scraper;
 
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Read};
 use std::fs::File;
 use std::path::Path;
+use std::collections::HashMap;
+use serde::Deserialize;
+
 
 fn main() {
     download("https://overrustlelogs.net/Destinygg%20chatlog/February%202022/2022-02-22.txt",
         "2022-02-22.txt");
-    download("https://cdn.destiny.gg/emotes/emotes.json", "emotes.txt");
+    download("https://cdn.destiny.gg/emotes/emotes.json", "emotes.json");
 
-    // read the file and print each line
-    if let Ok(lines) = read_lines("./emotes.txt") {
+    // Go through current emote list
+
+
+    // Go thru chat
+    if let Ok(lines) = read_lines("./2022-02-22.txt") {
         for line in lines {
-            if let Ok(messageInfo) = line {
-                println!("{}", messageInfo);
+            if let Ok(message) = line {
+                // do something with message
+
             }
         }
     }
-
 }
 
 /*
@@ -32,10 +38,34 @@ fn download(url: &str, name: &str) {
     let body = resp.text().expect("Body is invalid");
     let mut out = File::create(name).expect("Failed to create file");
     io::copy(&mut body.as_bytes(), &mut out).expect("Failed to copy content");
+
 }
 
 fn read_lines<T>(filename: T) -> io::Result<io::Lines<io::BufReader<File>>>
     where T: AsRef<Path>, {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
+}
+
+struct User {
+    emote_use: HashMap<String, i32>,
+    username: String
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct EmoteInfo{
+    twitch: bool,
+    theme: i32,
+    image: ImageInfo,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct ImageInfo {
+    url: String,
+    name: String,
+    mime: String,
+    height: i32,
+    width: i32,
 }
